b12a01bab2ab89fa76a1bcf1245d0f4bbe655bb0
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-02-23 15:39:44 +0200

    updated

--- src/UserProfileCard.js
+++ src/UserProfileCard.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import profilePicture from "../images/profile.jpg";
+import profilePicture from "../src/images/profile.jpg";
+
+
 function UserProfileCard() {
     
     return (
         <div className="user-profile-card">
            <img className="profilePicture" src={profilePicture} alt="Nicolette" />
             <h2>Nicolette Steenkamp</h2>
-            <p>As a junior software engineer, I bring a passion for problem-solving and a solid foundation in programming principles to the table. With a keen eye for detail and a drive to learn and adapt, I am committed to delivering high-quality solutions that meet both user needs and technical requirements.
+            <h3>software Engineer | Fullstack devloper</h3>
+            <h4>Technologies: HTML,CSS,Javascript,React</h4>
+            <p>As a junior software engineer, I bring a passion for problem-solving and a solid foundation in programming principles to the table. With a keen eye for detail and a drive to learn and adapt, I am committed to delivering high-quality solutions.
 
-My academic background, combined with practical experience gained through i personal projects, has equipped me with proficiency in various programming languages and development tools. I am comfortable collaborating with cross-functional teams and contributing positively to project objectives.
+My academic background, combined with practical experience gained through personal projects, has equipped me with proficiency in various programming languages and development tools.
 
 Eager to further develop my skills and contribute to innovative projects.</p>
-        </div>
+        
+
+    
+</div>
     );
 }
 
