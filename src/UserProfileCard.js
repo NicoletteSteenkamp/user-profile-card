8bfcb406681fbe02ff7fe64c4584cc62f2877080
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-02-23 12:04:51 +0200

    updated code

--- src/UserProfileCard.js
+++ src/UserProfileCard.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import './image';
+import './images';
 function UserProfileCard() {
+    
     return (
         <div className="user-profile-card">
-            <img src={'./profile.jpeg'} alt="Profile" />
+             <img src="images/profile.jpg" alt="Profile" />
             <h2>Nicolette Steenkamp</h2>
-            <p>A short bio about John Doe...</p>
+            <p>As a junior software engineer, I bring a passion for problem-solving and a solid foundation in programming principles to the table. With a keen eye for detail and a drive to learn and adapt, I am committed to delivering high-quality solutions that meet both user needs and technical requirements.
+
+My academic background, combined with practical experience gained through i personal projects, has equipped me with proficiency in various programming languages and development tools. I am comfortable collaborating with cross-functional teams and contributing positively to project objectives.
+
+Eager to further develop my skills and contribute to innovative projects, I am enthusiastic about embracing new challenges and opportunities for growth in the dynamic field of software engineering..</p>
         </div>
     );
 }
