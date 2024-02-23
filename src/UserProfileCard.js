37a491cec4e9be3781ae75216268c2192a519d2b
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-02-23 14:30:15 +0200

    pfp

--- src/UserProfileCard.js
+++ src/UserProfileCard.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import profilePicture from "../images/Profile-Picture.jpg";
 function UserProfileCard() {
     
     return (
         <div className="user-profile-card">
-             <img src="images/profile.jpg" alt="Profile" />
+           <img className="profilePicture" src={profilePicture} alt="Nicolette" />
             <h2>Nicolette Steenkamp</h2>
             <p>As a junior software engineer, I bring a passion for problem-solving and a solid foundation in programming principles to the table. With a keen eye for detail and a drive to learn and adapt, I am committed to delivering high-quality solutions that meet both user needs and technical requirements.
 
