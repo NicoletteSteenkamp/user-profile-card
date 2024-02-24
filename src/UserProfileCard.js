0c1e4ae74ae58027161437bb1326c62121bc20a5
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-02-24 06:47:07 +0200

    fixed typos

--- src/UserProfileCard.js
+++ src/UserProfileCard.js
@@ -8,9 +8,9 @@ function UserProfileCard() {
         <div className="user-profile-card">
            <img className="profilePicture" src={profilePicture} alt="Nicolette" />
             <h2>Nicolette Steenkamp</h2>
-            <h3>software Engineer | Fullstack devloper</h3>
+            <h3>Software Engineer | Fullstack Developer</h3>
             <h4>Technologies: HTML,CSS,Javascript,React</h4>
-            <p>As a junior software engineer, I bring a passion for problem-solving and a solid foundation in programming principles to the table. With a keen eye for detail and a drive to learn and adapt, I am committed to delivering high-quality solutions.
+            <p>As a junior Software Engineer, I bring a passion for problem-solving and a solid foundation in programming principles to the table. With a keen eye for detail and a drive to learn and adapt, I am committed to delivering high-quality solutions.
 
 My academic background, combined with practical experience gained through personal projects, has equipped me with proficiency in various programming languages and development tools.
 
